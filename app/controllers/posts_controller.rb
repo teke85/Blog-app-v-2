48c2793c849db608b1a823c53d8d984410bfa00e
Nelofar Zabi <mutau85@gmail.com>
2023-07-28 16:15:20 +0200

    Fix linters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @posts = @user.posts # Assuming there is an association between User and Post models
   end
-  
+
   def show
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
