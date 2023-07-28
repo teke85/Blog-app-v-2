75b476ec8829f49311e8fb1f88921aa4870b4c35
Nelofar Zabi <mutau85@gmail.com>
2023-07-28 16:09:11 +0200

    Add index action to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,9 @@
 class PostsController < ApplicationController
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts # Assuming there is an association between User and Post models
+  end
+  
   def show
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
