6987c31b088955998e91dff469dceb8cacfd72ff
Nelofar Zabi <mutau85@gmail.com>
2023-07-28 12:38:19 +0200

    Add controllers, Add routes

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  def home
+    # Your logic for the home action goes here
+    # For example, you can set instance variables and render a view:
+    @welcome_message = "Welcome to our blog!"
+  end
+end
