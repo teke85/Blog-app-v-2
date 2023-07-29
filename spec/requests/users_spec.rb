a2e0d2893539f732387cfe480dc4ce8049942d44
Nelofar Zabi <mutau85@gmail.com>
2023-07-29 05:05:42 +0200

    Fix linter error, Add one more controller test

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -6,6 +6,16 @@ RSpec.describe 'Users', type: :request do
       get users_path
       expect(response).to render_template('index')
     end
+  end
+
+  describe "User GET /show" do
+    # Create a user before the test
+    let!(:user) { User.create(name: "John Doe") }
+
+    it 'returns success for detail user' do
+      get "/users/#{user.id}"
+      expect(response).to have_http_status(200)
+    end
 
     it 'includes correct placeholder text' do
       get users_path
