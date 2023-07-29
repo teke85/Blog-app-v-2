6bcba23b85b0e9dff9be56c12772690b07628369
Nelofar Zabi <mutau85@gmail.com>
2023-07-29 05:18:16 +0200

    Perform Linter Check

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe 'Users', type: :request do
       expect(response).to have_http_status(200)
     end
 
-     it 'user responsed body with correct place holder' do
+    it 'user responsed body with correct place holder' do
       get '/users/'
       expect(response.body).to include('<h1>All users page placeholder</h1>')
     end
