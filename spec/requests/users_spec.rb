20dcc1c847e282e4bfb74b522f002467e125139b
Nelofar Zabi <mutau85@gmail.com>
2023-07-29 05:17:37 +0200

    Add controller test to users file

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -17,6 +17,11 @@ RSpec.describe 'Users', type: :request do
       expect(response).to have_http_status(200)
     end
 
+     it 'user responsed body with correct place holder' do
+      get '/users/'
+      expect(response.body).to include('<h1>All users page placeholder</h1>')
+    end
+
     it 'includes correct placeholder text' do
       get users_path
       expect(response.body).to include('<h1>All users page placeholder</h1>')
